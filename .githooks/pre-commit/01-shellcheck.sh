b56e93dbca41f007710d28f7c9d0f53ce644f146
Giovanni Bassi <giggio@giggio.net>
2024-01-23 19:49:31 -0300

    Update pre-commit git hook

--- .githooks/pre-commit/01-shellcheck.sh
+++ .githooks/pre-commit/01-shellcheck.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-find "$(pwd)" -name '*.sh' -exec shellcheck --shell bash {} +
+submodule_dirs=$(git submodule | awk '{ printf "-not -path '"$(pwd)"'/" $2 "/* " }')
+set -f # disable globbing
+# shellcheck disable=SC2086
+files=$(find "$(pwd)" -name '*.sh' $submodule_dirs)
+# shellcheck disable=SC2086
+shellcheck --shell bash $files
+set +f
