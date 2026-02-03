56eb4f4e5198b429bfb241dfcaf4e269c51246f1
Giovanni Bassi <giggio@giggio.net>
2026-02-03 18:17:31 -0300

    Add flake and improve test helper

--- tests/_test-helpers.sh
+++ tests/_test-helpers.sh
@@ -1,18 +1,22 @@
-BATS_LOCATION=`which bats`;
+BATS_LOCATION=$(which bats)
 BATS_LOCATION="${BATS_LOCATION%/*/*/*/*}"
-if ! [ -f "$BATS_LOCATION/bats-assert/load.bash" ]; then
-  npm install -g bats-assert
+if ! bats_load_library bats-assert; then
+  if ! [ -f "$BATS_LOCATION/bats-assert/load.bash" ]; then
+    npm install -g bats-assert
+  fi
+  load "$BATS_LOCATION/bats-assert/load.bash"
 fi
-load "$BATS_LOCATION/bats-assert/load.bash"
-if ! [ -f "$BATS_LOCATION/bats-support/load.bash" ]; then
-  npm install -g bats-support
+if ! bats_load_library bats-support; then
+  if ! [ -f "$BATS_LOCATION/bats-support/load.bash" ]; then
+    npm install -g bats-support
+  fi
+  load "$BATS_LOCATION/bats-support/load.bash"
 fi
-load "$BATS_LOCATION/bats-support/load.bash"
 SUPPORT_DIR=$BATS_TEST_DIRNAME/.testsupport
 if ! [ -f "$SUPPORT_DIR"/bats-mock.bash ]; then
   if ! [ -d "$SUPPORT_DIR" ]; then
     mkdir "$SUPPORT_DIR"
   fi
-  \curl -fsSL  --output "$SUPPORT_DIR"/bats-mock.bash https://raw.githubusercontent.com/grayhemp/bats-mock/master/src/bats-mock.bash
+  \curl -fsSL --output "$SUPPORT_DIR"/bats-mock.bash https://raw.githubusercontent.com/grayhemp/bats-mock/master/src/bats-mock.bash
 fi
 load "$SUPPORT_DIR"/bats-mock.bash
